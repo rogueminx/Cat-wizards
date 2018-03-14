c2c740b762acf9002175237cf8ddc74f3ebd6f77
Anna Marie Cooper and John Rykken <john@>
2018-03-14 14:44:12 -0700

    lower health points with strikes against enemy. Pass all specs.

--- spec/fight_spec.js
+++ spec/fight_spec.js
@@ -1,6 +1,7 @@
 import { Wizard } from '../src/wizard.js';
 import { Necromancer } from '../src/necromancer.js';
-import { Fight } from '../src/fight.js';
+import { Fight, attackValue } from '../src/fight.js';
+
 
 describe('fight', function() {
   var wizard;
@@ -13,14 +14,22 @@ describe('fight', function() {
     fight = new Fight(wizard, necromancer);
   });
 
+// TEST 1
   it('creates a new instance of the fight class', function() {
     expect(fight.wizard.strength).toEqual(4);
     expect(fight.necro.dog).toEqual('hell hound');
   });
 
+// TEST 2
   it('returns an attack value', function() {
-    var attackVal = fight.attackValue(wizard.strength);
+    var attackVal = attackValue(wizard.strength);
     expect(attackVal.toString()).toMatch(/[4-9]|[1][0-9]|[2][0-4]/);
   });
 
-});
+// TEST 3
+  it('allows attacker to strike defender and decrease defender health points', function(){
+    fight.strike();
+    expect(fight.necro.health).toBeLessThan(357);
+  });
+
+}); // describe fight
--- src/fight.js
+++ src/fight.js
@@ -1,24 +1,37 @@
 import { Wizard } from '../src/wizard.js';
 import { Necromancer } from '../src/necromancer.js';
 
+export function attackValue(strength) {
+  var attackVal = Math.floor(Math.random()*6)+1;
+  if (strength === 1) {
+    return attackVal;
+  } else if (strength === 2) {
+    return (attackVal * 2);
+  } else if (strength === 3) {
+    return (attackVal * 3);
+  } else {
+    return (attackVal * 4);
+  }
+} // attack
 
 export class Fight {
   constructor(wizard, necro) {
     this.wizard = wizard;
     this.necro = necro;
+    this.turn = true;
   }
 
-  attackValue(strength) {
-    var attackVal = Math.floor(Math.random()*6)+1;
-    if (strength === 1) {
-      return attackVal;
-    } else if (strength === 2) {
-      return (attackVal * 2);
-    } else if (strength === 3) {
-      return (attackVal * 3);
+
+  strike() {
+    var wizardAttack = attackValue(this.wizard.strength);
+    var necroAttack = attackValue(this.necro.strength);
+    if (this.turn === true) {
+      this.necro.health -= wizardAttack;
+      return this.necro.health;
     } else {
-      return (attackVal * 4);
+      this.wizard.health -= necroAttack;
+      return this.wizard.health;
     }
-  } // attack
+  } // strike
 
 } //Fight
