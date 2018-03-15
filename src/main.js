ee9818cd213f88ecfd5808a5e710859bf60ccc3a
Anna Marie Cooper and John Rykken <john@>
2018-03-15 11:52:10 -0700

    add basic HTML structure.

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Wizard } from './wizard.js';
+import { Necromancer } from './necromancer.js';
+import { Fight } from './fight.js';
