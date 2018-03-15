8b97b6e50e35747c105b6804404e9febe2d2cc08
Anna Marie Cooper and John Rykken <anna@>
2018-03-15 15:50:05 -0700

    Add UI functionality. Levelup. Death. Strike. Changeturn.

--- src/main.js
+++ src/main.js
@@ -5,27 +5,40 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Wizard } from './wizard.js';
 import { Necromancer } from './necromancer.js';
 import { Fight } from './fight.js';
-import Picture from './wizard1.jpg' // image code
-
-
 
 $(document).ready(function() {
-  var myPicture = new Image(); // image code
-  myPicture.src = Picture; // image code
-  picture.appendChild(myPicture); // image code
   var fight;
   var wizard;
   var necro;
   wizard = new Wizard(1, 'kitten', 50);
   necro = new Necromancer(1, 'puppy', 45);
   fight = new Fight(wizard, necro);
-  $('#wizardstrength').append(wizard.strength);
-  $('#wizardcat').append(wizard.cat);
-  $('#wizardhealth').append(wizard.health);
-  $('#necrostrength').append(necro.strength);
-  $('#necrodog').append(necro.dog);
-  $('#necrohealth').append(necro.health);
+
   $('#play').click(function(){
+    var skill = fight.wizard.skill;
     fight.strike();
+    fight.changeTurn();
+    fight.death();
+    console.log("This is the wizard skill " + skill);
+
+    // level up
+    if (skill === 5) {
+      fight.levelUp();
+    } else if (skill === 10) {
+      fight.levelUp();
+    } else if (skill === 20) {
+      fight.levelUp();
+    } else {
+      console.log("Your leveling up is being ignored.")
+    }
+
+    $('#necrostrength').text(fight.necro.strength);
+    $('#necrodog').text(fight.necro.dog);
+    $('#necrohealth').text(fight.necro.health);
+    $('#wizardstrength').text(fight.wizard.strength);
+    $('#wizardcat').text(fight.wizard.cat);
+    $('#wizardhealth').text(fight.wizard.health);
+
+    console.log(fight.wizard.skill);
   });
 });
